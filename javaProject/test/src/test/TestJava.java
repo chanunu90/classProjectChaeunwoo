d35609aff86185bbe8ef074e66f1a4262b302707
himediaEUNWOOCHA <128343652+himediaEUNWOOCHA@users.noreply.github.com>
2023-03-20 22:09:15 +0900

    Update TestJava.java

--- javaProject/test/src/test/TestJava.java
+++ javaProject/test/src/test/TestJava.java
@@ -3,6 +3,7 @@ package test;
 public class TestJava {
 
 	public static void main(String[] args) {
-		System.out.println("First Java 차은우 배고파 ");
+		System.out.println("First Java 차은우 배고파 d집에서 테스트");
+		System.out.println("배가 너무 고프다 그래서 소고기를 먹었다 한글 인코딩이 안될떄는 MS949를 쓴다");
 	}
 }
