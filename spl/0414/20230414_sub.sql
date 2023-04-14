f8da02ce0e6c137f5e2d1a9a4075c9ab049571ea
himediaEUNWOOCHA <vltfhem1206@gmail.com>
2023-04-14 19:35:27 +0900

    일단 업글 집가서 해보자
    일단 업글 집가서 해보자

--- spl/0414/20230414_sub.sql
+++ spl/0414/20230414_sub.sql
@@ -2,15 +2,37 @@
 select ename , job
 from emp
 where job = (select job from emp where empno=7788);
+
 --44. 사원번호가 7499인 사원보다 급여가 많은 사원을 표시하시오. 사원이름과 감당 업무
-select *
-from asd;
+select ename , job
+from emp
+where SAL > (select SAL from emp where empno=7499);
+
 --45. 최소급여를 받는 사원의 이름, 담당업무 및 급여를 표시하시오. (그룹함수 사용)
---
+select ENAME , job , SAL
+from emp
+WHERE SAL = (
+    select MIN(SAL)
+    from emp
+);
+
 --46. 평균급여가 가장 적은 직급의 직급 이름과 직급의 평균을 구하시오.
---
+SELECT JOB , AVG(SAL) 
+FROM EMP
+WHERE JOB = (
+    SELECT JOB
+    FROM (select job , AVG(SAL) AS D from emp GROUP BY job ORDER BY AVG(SAL))
+    WHERE ROWNUM = 1
+)
+GROUP BY JOB;
+
 --47. 각 부서의 최소 급여를 받는 사원의 이름, 급여, 부서번호를 표시하시오.
---
+SELECT E1.DEPTNO , MIN(E1.SAL)
+FROM EMP E1 , EMP E2
+WHERE E1.DEPTNO = E2.DEPTNO
+GROUP BY E1.DEPTNO;
+
+
 --48. 담당업무가 ANALYST 인 사원보다 급여가 적으면서 업무가 ANALYST가 아닌 사원들을 표시(사원번호, 이름, 담당 업무, 급여)하시오.
 --
 --49. 부하직원이 없는 사원의 이름을 표시하시오.
